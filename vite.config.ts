ebea57a9827dd87ac9e1a869e831c2cc7d0032b8
Vladimir Yazykov <neizerth@gmail.com>
2024-09-03 23:43:08 +0300

    added deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import 'dotenv/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +12,11 @@ export default defineConfig({
     }
   },
   plugins: [react()],
+  base: process.env.APP_BASE_PATH || '/ArkhamDivider/',
+  build: {
+    outDir: process.env.APP_BUILD_DIR || 'dist'
+  },
+  preview: {
+    port: Number(process.env.APP_PREVIEW_PORT) || 8080
+  }
 })
