eb368fd78ff5b9e167b359073ba31d079b9ba685
Vladimir Yazykov <neizerth@gmail.com>
2024-09-04 00:35:10 +0300

    fix

--- src/components/filters/CampaignSelect/CampaignSelect.tsx
+++ src/components/filters/CampaignSelect/CampaignSelect.tsx
@@ -56,7 +56,7 @@ export const CampaignSelect = ({ className }: CampaignSelectProps) => {
   }
 
   return (
-    <Row className={classNames(S.container, className)}>
+    <Row wrap className={classNames(S.container, className)}>
       <Select 
           className={S.select}
           onChange={item => updateCampaign(item?.value || null)} 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './index.css'
+import './index.scss'
 
 import { StoreProvider } from '@/providers/StoreProvider.tsx';
 import { I18nextProvider } from 'react-i18next';
