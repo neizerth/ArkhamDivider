03707c984347fc83e27f39ffde0941390613ce16
Vladimir Yazykov <neizerth@gmail.com>
2024-09-23 07:46:54 +0300

    fix

--- src/components/dividers/classic/ClassicDivider/backgrounds.ts
+++ src/components/dividers/classic/ClassicDivider/backgrounds.ts
@@ -4,8 +4,8 @@ import { LayoutOrientation } from '@/types/layouts';
 import horizontalBgColor from './images/horizontal.png'
 import horizontalBgGrayscale from './images/horizontal_bw.png'
 
-import verticalBgColor from './images/horizontal.png'
-import verticalBgGrayscale from './images/horizontal.png'
+import verticalBgColor from './images/vertical.png'
+import verticalBgGrayscale from './images/vertical_bw.png'
 
 export const backgrounds = [
   {
