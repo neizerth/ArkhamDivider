d44235f823da5e1125ff131281304d19d6b14628
Vladimir Yazykov <neizerth@gmail.com>
2024-10-15 16:29:14 +0300

    added metrics

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import path from 'path';
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 import { defineConfig } from 'vite';
 import svgr from "vite-plugin-svgr";
 import react from '@vitejs/plugin-react';
 import 'dotenv/config';
+
+const metrica = ((id?: string) => {
+  if (!id) {
+    return;
+  }
+  return {
+    id
+  }
+})(process.env.APP_METRIKA_ID) ;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +22,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  plugins: [svgr(), react()],
+  plugins: [
+    svgr(), 
+    react(),
+    VitePluginRadar({
+      metrica
+    })
+  ],
   base: process.env.APP_BASE_PATH || '/ArkhamDivider/',
   build: {
     outDir: process.env.APP_BUILD_DIR || 'dist'
