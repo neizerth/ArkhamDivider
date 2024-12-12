47c19013ea26fd1bd3349fb58975a5eb03ad0a17
Vladimir Yazykov <neizerth@gmail.com>
2024-12-12 23:49:56 +0300

    updated vintage layout

--- src/data/layouts/vintage.ts
+++ src/data/layouts/vintage.ts
@@ -24,7 +24,6 @@ export const vintageLayout: ILayout = {
 
 export const vintageLayoutCategory: ILayoutCategory = {
   id: 'vintage',
-  unlisted: true,
   name: 'Vintage (Tabbed)',
   info: 'Tabbed Vintage Dividers',
   author: {
