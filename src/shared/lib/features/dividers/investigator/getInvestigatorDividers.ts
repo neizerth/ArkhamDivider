222b645a75431abafb99ec41147ccd7ede7618f9
Vladimir Yazykov <neizerth@gmail.com>
2025-04-29 18:00:37 +0300

    build fix

--- src/shared/lib/features/dividers/investigator/getInvestigatorDividers.ts
+++ src/shared/lib/features/dividers/investigator/getInvestigatorDividers.ts
@@ -2,39 +2,39 @@ import type { IInvestigator } from "@/shared/types/api";
 import { DividerType, type IDivider } from "@/shared/types/dividers";
 import { uniqId } from "@/shared/lib/features/util/common";
 import factions from "@/shared/data/factions.json";
-import { groupBy, isNotNil, prop, propEq, values } from "ramda";
+import { groupBy, isNotNil, propEq, values } from "ramda";
 
 export const getInvestigatorDividers = ({
-	investigators,
+  investigators,
 }: {
-	investigators: IInvestigator[];
+  investigators: IInvestigator[];
 }): IDivider[] => {
-	const investigatorGroups = groupBy(
-		({ faction_code, name }) => `${name}-${faction_code}`, 
-		investigators
-	);
-	const data = values(investigatorGroups)
-		.filter(isNotNil)
-		.map((group) => group[0]);
+  const investigatorGroups = groupBy(
+    ({ faction_code, name }) => `${name}-${faction_code}`,
+    investigators
+  );
+  const data = values(investigatorGroups)
+    .filter(isNotNil)
+    .map((group) => group[0]);
 
-	return data
-		.map((investigator) => {
-			const { name, faction_code } = investigator;
+  return data
+    .map((investigator) => {
+      const { name, faction_code } = investigator;
 
-			const faction = factions.find(propEq(faction_code, "id"));
-			if (!faction) {
-				return;
-			}
-			const { icon } = faction;
-			return {
-				id: uniqId(),
-				faction: faction.id,
-				investigator,
-				type: DividerType.INVESTIGATOR,
-				specialIcon: "per_investigator",
-				name,
-				icon,
-			};
-		})
-		.filter(isNotNil);
+      const faction = factions.find(propEq(faction_code, "id"));
+      if (!faction) {
+        return;
+      }
+      const { icon } = faction;
+      return {
+        id: uniqId(),
+        faction: faction.id,
+        investigator,
+        type: DividerType.INVESTIGATOR,
+        specialIcon: "per_investigator",
+        name,
+        icon,
+      };
+    })
+    .filter(isNotNil);
 };
