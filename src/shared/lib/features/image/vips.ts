34b9b3c22f62abdf2aa089352a2d2bae29659498
Vladimir Yazykov <neizerth@gmail.com>
2025-05-01 08:39:02 +0300

    test: increase vips memory

--- src/shared/lib/features/image/vips.ts
+++ src/shared/lib/features/image/vips.ts
@@ -3,9 +3,12 @@ import Vips from "wasm-vips";
 
 let vips: Nullable<typeof Vips> = null;
 
+const Mb = 1024;
+
 export const getVips = async () => {
-	if (!vips) {
-		vips = await Vips();
-	}
-	return vips;
+  if (!vips) {
+    vips = await Vips();
+    vips.Cache.maxMem(128 * Mb);
+  }
+  return vips;
 };
