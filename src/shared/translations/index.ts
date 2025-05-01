92a83807ca3126048d5f0014b72ab908e40c2d2c
Vladimir Yazykov <neizerth@gmail.com>
2025-05-01 22:02:39 +0300

    added it dummy translation

--- src/shared/translations/index.ts
+++ src/shared/translations/index.ts
@@ -4,3 +4,4 @@ export { default as fr } from "./fr.json";
 export { default as pl } from "./pl.json";
 export { default as es } from "./es.json";
 export { default as ko } from "./ko.json";
+export { default as it } from "./it.json";
