c6abdd9c68676834f54108910c3206d2b6747a05
Vladimir Yazykov <neizerth@gmail.com>
2025-05-07 20:35:28 +0300

    fix: duplicated types

--- src/shared/lib/features/dividers/story/getStoryDividers.ts
+++ src/shared/lib/features/dividers/story/getStoryDividers.ts
@@ -1,44 +1,47 @@
 import { IEncounterSet, IStory } from "@/shared/types/api";
 import { IDivider } from "@/shared/types/dividers";
 import { arrayIf } from "@/shared/lib/features/util/common";
-import { ascend, prop, sortWith } from "ramda";
+import { ascend, prop, sortWith, uniqBy } from "ramda";
 import { getScenarioDividers } from "./getScenarioDividers";
 import { getEncounterDividers } from "./getEncounterDividers";
 import { AddStoryDividersOptions } from "@/shared/store/features/addDividers/addDividers";
 import { getCampaignDividers } from "./getCampaignDividers";
 
 export type IGetStoryDividersOptions = AddStoryDividersOptions & {
-	encounterSets: IEncounterSet[];
-	returnStories?: IStory[];
+  encounterSets: IEncounterSet[];
+  returnStories?: IStory[];
 };
 
 export const getStoryDividers = (options: IGetStoryDividersOptions) => {
-	const { returnStories = [], includeScenarios } = options;
+  const { returnStories = [], includeScenarios } = options;
 
-	const scenarioDividers: IDivider[] = getScenarioDividers(options);
+  const scenarioDividers: IDivider[] = getScenarioDividers(options);
 
-	const encounterDividers: IDivider[] = getEncounterDividers({
-		...options,
-	});
+  const encounterDividers: IDivider[] = getEncounterDividers({
+    ...options,
+  });
 
-	const returnSetDividers: IDivider[] = returnStories
-		.map((story) =>
-			getStoryDividers({
-				...options,
-				story,
-				returnStories: [],
-			}),
-		)
-		.flat();
+  const returnSetDividers: IDivider[] = returnStories.flatMap((story) =>
+    getStoryDividers({
+      ...options,
+      story,
+      returnStories: [],
+    })
+  );
 
-	const campaignDividers: IDivider[] = getCampaignDividers(options);
+  const campaignDividers: IDivider[] = getCampaignDividers(options);
 
-	const dividers = [
-		...campaignDividers,
-		...arrayIf(includeScenarios, scenarioDividers),
-		...encounterDividers,
-		...returnSetDividers,
-	];
+  const dividers = [
+    ...campaignDividers,
+    ...arrayIf(includeScenarios, scenarioDividers),
+    ...encounterDividers,
+    ...returnSetDividers,
+  ];
 
-	return sortWith([ascend(prop("type"))], dividers);
+  const uniqueDividers = uniqBy(
+    ({ icon, type }) => `${type}_${icon}`,
+    dividers
+  );
+
+  return sortWith([ascend(prop("type"))], uniqueDividers);
 };
