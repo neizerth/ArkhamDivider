1e793034bc433bf015e8a1eac0049b2a2d6defef
Vladimir Yazykov <neizerth@gmail.com>
2025-09-11 08:11:23 +0300

    fix: csp

--- src/shared/lib/features/image/vips.ts
+++ src/shared/lib/features/image/vips.ts
@@ -1,5 +1,5 @@
 import { Nullable } from "@/shared/types/util";
-import Vips from "wasm-vips";
+import type Vips from "wasm-vips";
 
 let vips: Nullable<typeof Vips> = null;
 
@@ -10,6 +10,7 @@ const Mb = Kb * 1024;
 const MAX_MEMORY = 128;
 
 export const getVips = async () => {
+
   const memoryUsage = vips ? vips.Stats.mem() / Mb : 0;
 
   if (memoryUsage > MAX_MEMORY) {
@@ -17,6 +18,7 @@ export const getVips = async () => {
   }
 
   if (!vips) {
+    const { default: Vips } = await import("wasm-vips");
     vips = await Vips();
     vips.Cache.maxMem(128 * Mb);
   }
