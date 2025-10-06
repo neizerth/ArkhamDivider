fe234b51bb8c5b646103f3a37c24c21a30bf88e2
Vladimir Yazykov <neizerth@gmail.com>
2025-10-06 09:00:58 +0300

    fix: hide not exportable when printing

--- src/components/ui/behavior/NotExportable/NotExportable.tsx
+++ src/components/ui/behavior/NotExportable/NotExportable.tsx
@@ -1,4 +1,4 @@
-// import S from './NotExportable.module.scss';
+import S from './NotExportable.module.scss';
 
 import { PropsWithChildren } from 'react';
 import { useAppSelector } from '@/shared/lib/hooks/useAppSelector';
@@ -6,5 +6,5 @@ import { selectExport } from '@/shared/store/features/app/app';
 
 export const NotExportable = ({ children }: PropsWithChildren) => {
   const isExport = useAppSelector(selectExport);
-  return <>{!isExport && children}</>;
+  return <div className={S.container}>{!isExport && children}</div>;
 };
