fd50751330c408e9f4869ad162309ac4e1a27542
Vladimir Yazykov <neizerth@gmail.com>
2025-11-02 21:10:15 +0300

    fix: selector caching

--- src/shared/store/features/dividers/selectors/selectStoryInvestigators.ts
+++ src/shared/store/features/dividers/selectors/selectStoryInvestigators.ts
@@ -1,29 +1,30 @@
-import type { AppSelector } from '@/shared/store';
-import type { IInvestigator, IStory } from '@/shared/types/api';
+import type { IStory } from '@/shared/types/api';
 import { Nullable } from '@/shared/types/util';
 import { selectIsArkhamesqueLayout } from '../../layout/layout';
-import { selectArkhamesqueClassicInvestigators as selectInvestigators } from '../arkhamesque/arkhamesque';
+import {
+  selectArkhamesqueData,
+  selectArkhamesqueClassicInvestigators as selectInvestigators,
+} from '../arkhamesque/arkhamesque';
+import { createSelector } from '@reduxjs/toolkit';
 
-export const selectStoryInvestigators =
-  (story: Nullable<IStory>): AppSelector<IInvestigator[]> =>
-  (state) => {
-    if (!story) {
-      return [];
+export const selectStoryInvestigators = (story: Nullable<IStory>) =>
+  createSelector(
+    [selectIsArkhamesqueLayout, selectInvestigators, selectArkhamesqueData],
+    (isArkhamesqueLayout, arkhamesqueInvestigators, data) => {
+      if (!story) {
+        return [];
+      }
+
+      if (!isArkhamesqueLayout) {
+        return story.investigators;
+      }
+
+      if (!data) {
+        return [];
+      }
+
+      return story.investigators.filter(({ code, alternate_of }) =>
+        arkhamesqueInvestigators.includes(alternate_of || code)
+      );
     }
-    const { data } = state.arkhamesque;
-    const isArkhamesqueLayout = selectIsArkhamesqueLayout(state);
-
-    if (!isArkhamesqueLayout) {
-      return story.investigators;
-    }
-
-    if (!data) {
-      return [];
-    }
-
-    const arkhamesqueInvestigators = selectInvestigators(state);
-
-    return story.investigators.filter(({ code, alternate_of }) =>
-      arkhamesqueInvestigators.includes(alternate_of || code)
-    );
-  };
+  );
