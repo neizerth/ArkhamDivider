78497b37e50256702db6b399f65d205e66844236
Vladimir Yazykov <neizerth@gmail.com>
2026-01-20 13:19:48 +0300

    fix: ui

--- slices/widgets/layout/SingleColumnLayout/SingleColumnLayout.components.ts
+++ slices/widgets/layout/SingleColumnLayout/SingleColumnLayout.components.ts
@@ -4,10 +4,10 @@ import { styled } from "@mui/material/styles";
 export const Main = styled("main")`
   flex-grow: 1;
   margin-bottom: ${({ theme }) => theme.spacing(8)};
-  background-color: #f9f9f9;
 `;
 
 export const Container = styled(Stack)(({ theme }) => ({
 	minHeight: "100vh",
 	paddingTop: theme.spacing(12),
+	backgroundColor: "#f9f9f9",
 }));
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { App } from "@/modules/core/app/app/ui";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "@/modules/core/app/app/ui";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
