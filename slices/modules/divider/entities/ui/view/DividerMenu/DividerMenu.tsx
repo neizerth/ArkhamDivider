25ec0d4f62531e06f122a38b6fa749fbbf1ba0db
Vladimir Yazykov <neizerth@gmail.com>
2026-02-04 20:48:26 +0100

    fix: deps

--- slices/modules/divider/entities/ui/view/DividerMenu/DividerMenu.tsx
+++ slices/modules/divider/entities/ui/view/DividerMenu/DividerMenu.tsx
@@ -51,7 +51,7 @@ export function DividerMenu({
 			);
 			setShowDownload.off();
 		},
-		[dispatch, dividerId, setShowDownload],
+		[dispatch, dividerId, setShowDownload.off],
 	);
 
 	const getPrintSx = usePrintSx();
