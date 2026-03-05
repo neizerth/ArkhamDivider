ed3365a5455354ec215fe73dc3cba943276e5322
Vladimir Yazykov <neizerth@gmail.com>
2026-03-05 11:46:55 +0300

    feat: tfa

--- slices/modules/core/icon/shared/config/position/campaign/index.ts
+++ slices/modules/core/icon/shared/config/position/campaign/index.ts
@@ -1,9 +1,11 @@
 import core from "./core";
 import dwl from "./dwl";
 import ptc from "./ptc";
+import tfa from "./tfa";
 
 export default {
 	...core,
 	...dwl,
 	...ptc,
+	...tfa,
 };
---
+++ slices/modules/core/icon/shared/config/position/campaign/tfa.ts
@@ -0,0 +1,123 @@
+import { percent } from "@/shared/util";
+import type { IconPositionManifest } from "../../../model";
+import { returnPosition } from "../util";
+
+export default {
+	the_forgotten_age: {
+		top: percent(-2),
+		left: percent(5),
+	},
+	return_to_the_forgotten_age: returnPosition,
+	agents_of_yig: {
+		left: percent(6),
+		scale: 0.97,
+	},
+	yigs_venom: {
+		top: percent(-2),
+		left: percent(2),
+		scale: 0.95,
+	},
+	temporal_flux: {
+		left: percent(4),
+		scale: 0.93,
+	},
+	deadly_traps: {
+		top: percent(-4),
+		left: percent(4),
+		scale: 0.98,
+	},
+	forgotten_ruins: {
+		left: percent(4),
+	},
+	poison: {
+		left: percent(5),
+	},
+	pillars_of_judgement: {
+		top: percent(-4),
+		left: percent(3),
+	},
+	rainforest: {
+		left: percent(5),
+		scale: 0.87,
+	},
+	serpents: {
+		top: percent(-3),
+		left: percent(4),
+	},
+	expedition: {
+		top: percent(-2),
+		left: percent(5),
+		scale: 1.05,
+	},
+	knyan: {
+		left: percent(4),
+		scale: 1.05,
+	},
+	pnakotic_brotherhood: {
+		left: percent(4),
+		scale: 1.05,
+	},
+	return_to_pillars_of_judgement: returnPosition,
+	return_to_the_rainforest: returnPosition,
+	doomed_expedition: {
+		left: percent(4),
+	},
+	return_to_knyan: returnPosition,
+	venomous_hate: {
+		top: percent(-2),
+		left: percent(4),
+	},
+	temporal_hunters: {
+		left: percent(5),
+		scale: 1.05,
+	},
+	cult_of_pnakotus: {
+		top: percent(-1),
+		left: percent(4),
+		scale: 0.85,
+	},
+	the_untamed_wilds: {
+		top: percent(2),
+		left: percent(5),
+		scale: 0.97,
+	},
+	return_to_the_untamed_wilds: returnPosition,
+	the_doom_of_eztli: {
+		left: percent(5),
+	},
+	return_to_the_doom_of_eztli: returnPosition,
+	threads_of_fate: {
+		left: percent(4),
+		scale: 0.95,
+	},
+	return_to_threads_of_fate: returnPosition,
+	the_boundary_beyond: {
+		top: percent(-6),
+		left: percent(4),
+		scale: 0.9,
+	},
+	return_to_the_boundary_beyond: returnPosition,
+	heart_of_the_elders: {
+		top: percent(-4),
+		left: percent(5),
+	},
+	return_to_the_heart_of_the_elders: returnPosition,
+	city_of_archives: {
+		left: percent(4),
+	},
+	return_to_city_of_archives: returnPosition,
+	the_depths_of_yoth: {
+		top: percent(5),
+		left: percent(4),
+	},
+	return_to_the_depths_of_yoth: returnPosition,
+	shattered_aeons: {
+		left: percent(5),
+	},
+	return_to_shattered_aeons: returnPosition,
+	turn_back_time: {
+		top: percent(-2),
+		left: percent(4),
+	},
+	return_to_turn_back_time: returnPosition,
+} as IconPositionManifest;
