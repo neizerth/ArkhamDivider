58617121fe5ea8c88135114b71f104f89a2e1186
Vladimir Yazykov <neizerth@gmail.com>
2026-03-11 09:21:41 +0300

    feat: zcp

--- slices/modules/core/icon/shared/config/position/custom/campaign/index.ts
+++ slices/modules/core/icon/shared/config/position/custom/campaign/index.ts
@@ -1,9 +1,11 @@
 import bloodborne from "./bloodborne";
 import zau from "./zau";
 import zaw from "./zaw";
+import zcp from "./zcp";
 
 export default {
 	...zau,
 	...zaw,
 	...bloodborne,
+	...zcp,
 };
---
+++ slices/modules/core/icon/shared/config/position/custom/campaign/zcp.ts
@@ -0,0 +1,86 @@
+import { percent } from "@/shared/util";
+import type { IconPositionManifest } from "../../../../model";
+
+export default {
+	call_of_the_plaguebearer: {
+		top: percent(-3),
+		scale: percent(90),
+	},
+	tenuous_allies: {
+		top: percent(-5),
+		left: percent(4),
+		scale: percent(95),
+	},
+	agency_survivors: {
+		top: percent(-2),
+		left: percent(3),
+		scale: percent(92),
+	},
+	gifts_of_the_plaguebearer: {
+		top: percent(-6),
+		left: percent(3),
+	},
+	life_and_death: {
+		top: percent(-1),
+		left: percent(3),
+	},
+	the_plaguebearers_commands: {
+		top: percent(-2),
+		left: percent(4),
+		scale: percent(95),
+	},
+	haze_of_miasma: {
+		top: percent(-4),
+		left: percent(5),
+	},
+	agents_of_cthugua: {
+		top: percent(-2),
+		left: percent(4),
+	},
+	fallen_arkham: {
+		top: percent(-6),
+		left: percent(3),
+	},
+	unbound_power: {
+		top: percent(-10),
+		left: percent(4),
+		scale: percent(95),
+	},
+	late_risers: {
+		top: percent(-6),
+		left: percent(3),
+	},
+	night_on_the_town: {
+		top: percent(-2),
+		scale: percent(90),
+	},
+	dead_by_dawn: {
+		top: percent(-5),
+		left: percent(6),
+		scale: percent(90),
+	},
+	mourning_stroll: {
+		top: percent(-6),
+		left: percent(2),
+		scale: percent(95),
+	},
+	high_noon_descent: {
+		top: percent(-2),
+		left: percent(4),
+		scale: percent(90),
+	},
+	the_afternoon_war: {
+		top: percent(-2),
+		left: percent(4),
+	},
+	death_at_sundown: {
+		top: percent(-4),
+		left: percent(4),
+		scale: percent(95),
+	},
+	the_midnight_hour: {
+		top: percent(-6),
+		left: percent(4),
+		scale: percent(90),
+	},
+} as IconPositionManifest;
