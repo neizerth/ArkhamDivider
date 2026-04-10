6f369219ea293a9e8f0b4bb4001c3e1211bc317e
Vladimir Yazykov <neizerth@gmail.com>
2026-04-10 15:57:08 +0300

    feat: footer

--- slices/widgets/layout/partials/footer/FooterMenu/FooterMenu.tsx
+++ slices/widgets/layout/partials/footer/FooterMenu/FooterMenu.tsx
@@ -1,5 +1,6 @@
 import type { JSX } from "react";
 import { useTranslation } from "react-i18next";
+import { PATREON_LINK } from "@/shared/config";
 import { Separator } from "@/shared/ui";
 import * as C from "./FooterMenu.components";
 
@@ -12,6 +13,10 @@ export function FooterMenu(props: FooterMenuProps) {
 			<C.Item to="/about">{t("About")}</C.Item>
 			<Separator />
 			<C.Item to="/how-to-print">{t("How to print")}</C.Item>
+			<Separator />
+			<C.Link href={PATREON_LINK} target="_blank">
+				Patreon
+			</C.Link>
 		</C.Container>
 	);
 }
