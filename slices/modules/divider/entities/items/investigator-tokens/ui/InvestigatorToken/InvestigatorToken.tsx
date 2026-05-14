0f1b28a17e2e35c81b6c138f990bddf9dd200cc2
Vladimir Yazykov <neizerth@gmail.com>
2026-05-14 10:27:53 +0300

    fix: tokens

--- slices/modules/divider/entities/items/investigator-tokens/ui/InvestigatorToken/InvestigatorToken.tsx
+++ slices/modules/divider/entities/items/investigator-tokens/ui/InvestigatorToken/InvestigatorToken.tsx
@@ -1,4 +1,5 @@
 import Box from "@mui/material/Box";
+import { useMemo } from "react";
 import {
 	DividerBackground as Background,
 	DividerContainer as Container,
@@ -11,28 +12,40 @@ import { InvesigatorTokensAPI } from "@/shared/api";
 import * as S from "./InvestigatorToken.styles";
 
 export function InvestigatorToken(props: DividerWithRelations) {
-	const getPrintSx = usePrintSx();
 	const borderRadius = useCornerRadius();
+
+	const { side } = props;
+	const sxProps = useMemo(
+		() => ({
+			side,
+		}),
+		[side],
+	);
+
+	const getPrintSx = usePrintSx(sxProps);
 
 	if (props.layoutType !== "investigator") {
 		console.log("not investigator");
 		return null;
 	}
-	const { side } = props;
 	const { code } = props.investigator;
 	const src = InvesigatorTokensAPI.getImageByCode(code);
 
 	const menuSx = getPrintSx(S.getMenuSx);
 	const containerSx = getPrintSx(S.containerSx);
-	const backgroundSx = getPrintSx(S.backgroundSx, { side });
+	const backgroundSx = getPrintSx(S.backgroundSx);
 
 	const outlineStyle = borderRadius ? "solid" : "none";
 
 	return (
 		<Container>
 			<Background src={src} sx={backgroundSx} />
+
+			{side === "front" && (
+				<Content sx={{ borderRadius: "50%", outlineStyle }} side="front" />
+			)}
 			<Box sx={containerSx}>
-				<Content sx={{ borderRadius: "50%", outlineStyle }} side={side}>
+				<Content hideBorderRadius side={side}>
 					<DividerMenu dividerId={props.id} sx={menuSx} />
 				</Content>
 			</Box>
