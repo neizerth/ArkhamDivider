7534cc0d920004f46c528edc449a03e50c06ad86
Vladimir Yazykov <neizerth@gmail.com>
2026-06-08 14:18:31 +0300

    fix: safari text entering

--- slices/shared/ui/control/FitInput/FitInput.tsx
+++ slices/shared/ui/control/FitInput/FitInput.tsx
@@ -1,5 +1,5 @@
 import type { SxProps } from "@mui/material/styles";
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import useFitText from "use-fit-text";
 import { BoxInput, type BoxInputProps } from "../BoxInput";
 import { useRemeasureOnFontsLoaded } from "./useRemeasureOnFontsLoaded";
@@ -14,26 +14,67 @@ export type FitInputProps = BoxInputProps & {
 	strokeSx?: SxProps;
 };
 
-export function FitInput({ fitTextOptions, ...props }: FitInputProps) {
+export function FitInput({
+	fitTextOptions,
+	onFocus: onFocusProp,
+	onBlur: onBlurProp,
+	...props
+}: FitInputProps) {
 	const { ref, fontSize } = useFitText(fitTextOptions);
 	useRemeasureOnFontsLoaded(ref as React.MutableRefObject<HTMLElement | null>);
+	const [isFocused, setIsFocused] = useState(false);
+	const frozenFontSizeRef = useRef(fontSize);
+
+	useEffect(() => {
+		if (!isFocused) {
+			frozenFontSizeRef.current = fontSize;
+		}
+	}, [fontSize, isFocused]);
 
 	useEffect(() => {
 		const value = Number(fontSize.replace("%", ""));
 		fitTextOptions?.onFontSizeChange?.(value);
 	}, [fontSize, fitTextOptions?.onFontSizeChange]);
 
+	const onFocus = useCallback<NonNullable<BoxInputProps["onFocus"]>>(
+		(event) => {
+			frozenFontSizeRef.current = fontSize;
+			setIsFocused(true);
+			onFocusProp?.(event);
+		},
+		[fontSize, onFocusProp],
+	);
+
+	const onBlur = useCallback<NonNullable<BoxInputProps["onBlur"]>>(
+		(event) => {
+			setIsFocused(false);
+			onBlurProp?.(event);
+		},
+		[onBlurProp],
+	);
+
+	const appliedFontSize = isFocused ? frozenFontSizeRef.current : fontSize;
+
 	const sx = {
 		...props.sx,
-		fontSize,
+		fontSize: appliedFontSize,
 		width: "100%",
 		height: "100%",
 	} as SxProps;
 
 	const strokeSx = {
 		...props.strokeSx,
-		fontSize,
+		fontSize: appliedFontSize,
 	} as SxProps;
 
-	return <BoxInput {...props} sx={sx} strokeSx={strokeSx} ref={ref} />;
+	return (
+		<BoxInput
+			{...props}
+			sx={sx}
+			strokeSx={strokeSx}
+			ref={ref}
+			onFocus={onFocus}
+			onBlur={onBlur}
+		/>
+	);
 }
