fcff3e1b75004430bb788408c210ff85eb04194c
Vladimir Yazykov <neizerth@gmail.com>
2026-06-17 10:39:21 +0300

    fix: arkhamesque

--- slices/modules/divider/entities/items/arkhamesque-classic/lib/logic/getArkhamesqueClassicInvestigators.ts
+++ slices/modules/divider/entities/items/arkhamesque-classic/lib/logic/getArkhamesqueClassicInvestigators.ts
@@ -2,5 +2,9 @@ import type { IArkhamesqueBuild } from "arkhamesque-classic-divider-data";
 import { prop } from "ramda";
 
 export const getArkhamesqueClassicInvestigators = (data: IArkhamesqueBuild) => {
-	return data.investigators.flatMap(({ data }) => data.map(prop("code")));
+	return (
+		data.investigators?.flatMap(
+			({ data: entries }) => entries?.map(prop("code")) ?? [],
+		) ?? []
+	);
 };
