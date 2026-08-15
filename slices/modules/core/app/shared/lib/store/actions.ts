46494fee0f14e5f4ee86987df5a1b210e847e90a
Vladimir Yazykov <neizerth@gmail.com>
2026-08-15 08:40:50 +0300

    refactor: translations load saga

--- slices/modules/core/app/shared/lib/store/actions.ts
+++ slices/modules/core/app/shared/lib/store/actions.ts
@@ -4,7 +4,3 @@ import type { ArkhamDivider } from "arkham-divider-data";
 export const appStarted = createAction("app/started");
 
 export const appDataLoaded = createAction<ArkhamDivider.Core>("app/dataLoaded");
-
-export const appTranslationsLoaded = createAction<ArkhamDivider.Translation>(
-	"app/translationsLoaded",
-);
