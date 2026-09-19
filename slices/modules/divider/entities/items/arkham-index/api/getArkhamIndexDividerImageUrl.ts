64dd47061ed1f846129ce8adc67b5f69a1c1f2ac
Vladimir Yazykov <neizerth@gmail.com>
2026-09-19 12:15:48 +0300

    fix: url gen

--- slices/modules/divider/entities/items/arkham-index/api/getArkhamIndexDividerImageUrl.ts
+++ slices/modules/divider/entities/items/arkham-index/api/getArkhamIndexDividerImageUrl.ts
@@ -63,5 +63,9 @@ export function getArkhamIndexDividerImageUrl({
 		return;
 	}
 
+	if (scenarioId === story.code) {
+		return `${scenarioBase}/${story.code}-scenario.avif`;
+	}
+
 	return `${scenarioBase}/${scenarioId}.avif`;
 }
